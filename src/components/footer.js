2712e546fcf2e279955ce45933f80a49a9a3b57b
Burhan Budak <budak.burhan@gmail.com>
2019-09-25 09:50:28 +0200

    abc

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,9 +7,9 @@ export default () => (
     <footer className={footerStyles.footer}>
         <h4>Links to my personal socialmedia</h4>
         <div className={footerStyles.middle}>
-            <div><a href="https://twitter.com/BurhanB98039015" target="_blank" rel="noopener"><FaTwitter/></a></div>
-            <div><a href="https://github.com/BurhanBudak" target="_blank"  rel="noopener"><FaGithub/></a></div>
-            <div><a href="https://www.linkedin.com/in/burhan-budak-6921b2130/" target="_blank"  rel="noopener"><FaLinkedinIn/></a></div>
+            <div><a href="https://twitter.com/BurhanB98039015" target="_blank" rel="noopener noreferrer"><FaTwitter/></a></div>
+            <div><a href="https://github.com/BurhanBudak" target="_blank"  rel="noopener noreferrer"><FaGithub/></a></div>
+            <div><a href="https://www.linkedin.com/in/burhan-budak-6921b2130/" target="_blank"  rel="noopener noreferrer"><FaLinkedinIn/></a></div>
         </div>
         <small>Skapare: 2019 © Burhan</small>
     </footer>

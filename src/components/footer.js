ebf3d6e0ae26bd3ce255a72b46253082b714f9ae
BurhanBudak <budak.burhan@gmail.com>
2022-01-28 12:34:33 +0100

    test

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,9 +7,9 @@ export default () => (
     <footer className={footerStyles.footer}>
         <h4>Links to my socialmedia and repo</h4>
         <div className={footerStyles.middle}>
-            <div><a href="https://twitter.com/BurhanB98039015" target="_blank" rel="noopener noreferrer"><FaTwitter/></a></div>
-            <div><a href="https://github.com/BurhanBudak" target="_blank"  rel="noopener noreferrer"><FaGithub/></a></div>
-            <div><a href="https://www.linkedin.com/in/burhan-budak-6921b2130/" target="_blank"  rel="noopener noreferrer"><FaLinkedinIn/></a></div>
+            <div><a href="https://twitter.com/BurhanB98039015" target="_blank" rel="noopener noreferrer" aria-label="twitter"><FaTwitter/></a></div>
+            <div><a href="https://github.com/BurhanBudak" target="_blank"  rel="noopener noreferrer" aria-label="github"><FaGithub/></a></div>
+            <div><a href="https://www.linkedin.com/in/burhan-budak-6921b2130/" target="_blank"  rel="noopener noreferrer" aria-label="linkedin"><FaLinkedinIn/></a></div>
         </div>
         <small>Copyright: 2019 © Burhan Budak</small>
     </footer>

ea1b7e12c5a49ee409d4cd50abde37a0108cc394
BurhanBudak <budak.burhan@gmail.com>
2022-01-28 14:42:08 +0100

    fixed footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,23 @@
 import React from "react"
-import { FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 import footerStyles from "./footer.module.css"
 
 
 export default () => (
-    <footer className={footerStyles.footer}>
-        <h4>Links to my socialmedia and repo</h4>
-        <div className={footerStyles.middle}>
-            <div><a href="https://twitter.com/BurhanB98039015" target="_blank" rel="noopener noreferrer"><FaTwitter/></a></div>
-            <div><a href="https://github.com/BurhanBudak" target="_blank"  rel="noopener noreferrer"><FaGithub/></a></div>
-            <div><a href="https://www.linkedin.com/in/burhan-budak-6921b2130/" target="_blank"  rel="noopener noreferrer"><FaLinkedinIn/></a></div>
-        </div>
-        <small>Copyright: 2019 © Burhan Budak</small>
-    </footer>
+  <footer className={footerStyles.footer}>
+    <h4>Links to my socialmedia and repo</h4>
+    <div className={footerStyles.middle}>
+      <div>
+        <a
+          href="https://github.com/BurhanBudak"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          <FaGithub />
+        </a>
+      </div>
+    </div>
+    <small>Copyright: 2019 © Burhan Budak</small>
+  </footer>
 )
